f6ec2bcbb936b14a56835661db8b9066fb74d17e
Maksim Lin <maks@manichord.com>
2022-01-12 17:09:58 +1100

    scaffold package

---
+++ bin/dart_repl.dart
@@ -0,0 +1,5 @@
+import 'package:dart_repl/dart_repl.dart' as dart_repl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_repl.calculate()}!');
+}
---
+++ test/dart_repl_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_repl/dart_repl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
